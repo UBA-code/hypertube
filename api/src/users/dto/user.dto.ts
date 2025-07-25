f576e0108c8dcc9b5b3c370cd504461e2912f150
UBA-code <belhachmiyassine4@gmail.com>
2025-07-25 22:58:20 +0100

    add a meaning error message in password validation failing

--- api/src/users/dto/user.dto.ts
+++ api/src/users/dto/user.dto.ts
@@ -42,13 +42,19 @@ export class UserDto {
 
   @ApiProperty()
   @IsNotEmpty()
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password must be have at least 8 characters, including 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol.',
+    },
+  )
   @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   password: string;
 }
